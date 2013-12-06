844ff5d9c0292d7528e08f20473807e4daa040ca
rraju1990 <rraju1990@gmail.com>
2013-12-06 21:26:49 +0530

    commented

--- CLASS-Engine/src/main/java/com/arima/classengine/engine/Analyzer.java
+++ CLASS-Engine/src/main/java/com/arima/classengine/engine/Analyzer.java
@@ -11,27 +11,68 @@ import com.arima.classengine.comparator.JaccardIndex;
 import com.arima.classengine.comparator.HammingDistance;
 
 public class Analyzer {
-
+    /**
+     * update existing models in central database / create new models if not already exists in the database
+     * @param year the year
+     * @param grade the grade to be predicted
+     * @param term the term to be predicted
+     * @param subject the subject to be predicted
+     * @throws Exception
+     */
 	public static void updateModel(int year, int grade, int term, String subject) throws Exception{
 		CAnalyzer.updateModel(year, grade, term, subject);
 	}
 
+    /**
+     * load the model from central database
+     * @param year the year
+     * @param grade the grade to be predicted
+     * @param term the term to be predicted
+     * @param subject the subject to be predicted
+     * @return
+     * @throws Exception
+     */
 	public static Model getModel(int year, int grade, int term, String subject) throws Exception{
 		return CAnalyzer.loadModelFromDatabase(year, grade, term, subject);
 	}
 
 
-	//it will return nearest profiles from same school (ySchool DB index numbers)
+    /**
+     * get nearest profiles within the same school
+     * @param schoolNo the school number to be searched
+     * @param grade the grade to be considered to match profiles
+     * @param term the term to be considered to match the profiles
+     * @param subjects the list of subjects to be considered to match the profiles
+     * @param marks the list of marks of a given student
+     * @return the list of nearest profiles' yschool index numbers
+     * @throws Exception
+     */
 	public static ArrayList<Integer> getNearestProfiles(int schoolNo, int grade, int term, List<String> subjects, List<Integer> marks) throws Exception{	
 		return NearestProfile.getNearestProfiles(schoolNo, grade, term, subjects, marks);
 	}
-	
-	//it will return nearest profiles from all schools (central DB index numbers)
+
+    /**
+     *  get nearest profiles among schools
+     * @param grade the grade to be considered to match profiles
+     * @param term the term to be considered to match the profiles
+     * @param subjects the list of subjects to be considered to match the profiles
+     * @param marks the list of marks of a given student
+     * @return the list of nearest profiles' class index numbers
+     * @throws Exception
+     */
 	public static ArrayList<Integer> getNearestProfiles(int grade, int term, List<String> subjects, List<Integer> marks) throws Exception{	
 		return NearestProfile.getNearestProfiles(grade, term, subjects, marks);
 	}
 
-
+    /**
+     * get the standard of the exams
+     * @param schoolNo the school number
+     * @param year the year of the exam to be compared
+     * @param grade the grade of the exam to be compared
+     * @param subject the subject to be compared
+     * @return an ExamStandard instance with overall similarity, individual similarity, count of results
+     * @throws Exception
+     */
     public static ExamStandard getExamStandard(int schoolNo, int year, int grade, String subject) throws Exception {
         return SimilarityMeasures.getExamStandard(schoolNo, year, grade, subject);
     }
--- CLASS-Engine/src/main/java/com/arima/classengine/engine/ExamStandard.java
+++ CLASS-Engine/src/main/java/com/arima/classengine/engine/ExamStandard.java
@@ -16,50 +16,98 @@ public class ExamStandard {
     private double jaccardIndex;
     private double sequenceAlignmentScore;
 
+    /**
+     *
+     * @return the array of results counts in term exam
+     */
     public int[] getTermCount() {
         return termCount;
     }
 
+    /**
+     *
+     * @param termCount the array of results counts in term exam
+     */
     public void setTermCount(int[] termCount) {
         this.termCount = termCount;
     }
 
+    /**
+     *
+     * @return the array of results counts in general exam
+     */
     public int[] getGeneralCount() {
         return generalCount;
     }
 
+    /**
+     *
+     * @param generalCount the array of results counts in general exam
+     */
     public void setGeneralCount(int[] generalCount) {
         this.generalCount = generalCount;
     }
 
+    /**
+     *
+     * @return the String of term exam results
+     */
     public String getTerm() {
         return term;
     }
 
+    /**
+     *
+     * @param term the String of term exam results
+     */
     public void setTerm(String term) {
         this.term = term;
     }
 
+    /**
+     *
+     * @return the String of general exam results
+     */
     public String getGeneral() {
         return general;
     }
 
+    /**
+     *
+     * @param general the String of general exam results
+     */
     public void setGeneral(String general) {
         this.general = general;
     }
 
+    /**
+     *
+     * @return the jaccard index
+     */
     public double getJaccardIndex() {
         return jaccardIndex;
     }
 
+    /**
+     *
+     * @param jaccardIndex the jaccard index
+     */
     public void setJaccardIndex(double jaccardIndex) {
         this.jaccardIndex = jaccardIndex;
     }
 
+    /**
+     *
+     * @return the sequence alignment score
+     */
     public double getSequenceAlignmentScore() {
         return sequenceAlignmentScore;
     }
 
+    /**
+     *
+     * @param sequenceAlignmentScore the sequence alignment score
+     */
     public void setSequenceAlignmentScore(double sequenceAlignmentScore) {
         this.sequenceAlignmentScore = sequenceAlignmentScore;
     }
